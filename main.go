4911fcb398ab4ebf90f5538e24e18d8511e04880
FriendlyUser <studentdavidli@gmail.com>
2018-09-01 21:16:35 -0700

    added basic circleCI

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import "os"
 
+func getEnv(key, fallback string) string {
+    if value, ok := os.LookupEnv(key); ok {
+        return value
+    }
+    return fallback
+}
+
 func main() {
 	app := App{}
 	app.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"),
-		os.Getenv("APP_DB_HOST"),
+		genENV("APP_DB_USERNAME","circleci"),
+		os.Getenv("APP_DB_PASSWORD",""),
+		os.Getenv("APP_DB_NAME","circle_test"),
+		os.Getenv("APP_DB_HOST","postgres"),
 		"5432",
 		"require")
 
